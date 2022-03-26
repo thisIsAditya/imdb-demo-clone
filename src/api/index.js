f0147bf2e0bdc33634ce2b0f57d614f9ad103962
Aditya Pandey <pandey.api2k@gmail.com>
2022-03-26 23:44:12 +0530

    Created Dynamic Movie Page

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export const baseImageURL = "https://image.tmdb.org/t/p/original"
 const API = axios.create({ baseURL: "https://movie-task.vercel.app/api" }); 
 export const getAllMovies = (page) => API.get(`/popular?${new URLSearchParams({page}).toString()}`)
-export const getSIngleMovies = (movieID) => API.get(`/movie?${new URLSearchParams({movieID}).toString()}`)
+export const getMovie = (movieId) => API.get(`/movie?${new URLSearchParams({movieId}).toString()}`)
 export const getSearchByNameResults = (page,query) => API.get(`/search?${new URLSearchParams({page, query}).toString()}`)
 
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,8 +1,86 @@
+import { useState } from "react";
+import { useParams } from "react-router";
+import { Col, Container, Row } from "react-bootstrap";
+import Loading from "../Utils/Loading.js";
+import ErrorComponent from "../Utils/Error.js";
+import {baseImageURL, getMovie} from "../../api";
+import Badge from 'react-bootstrap/Badge'
+import Card from "react-bootstrap/Card";
+import Stack from 'react-bootstrap/Stack'
+import Scroll from "../Utils/Scroll.js"
+
 const Movie = () => {
+    const {id} = useParams();
+
+    const [IsPending, setIsPending]=useState(true);
+    const [movie, setmovie]=useState(null);
+    const [Err, setErr]=useState(null);
+    useState(()=>{
+        const getData= async()=>{
+            const movieResponse = await getMovie(id)
+            if(movieResponse.status >= 400){
+                setErr(`Error Code : ${movieResponse.status} | Message : ${movieResponse.message}`)
+                setIsPending(false);
+            }else{
+                console.log(movieResponse.data.data, "THis is state");
+                setmovie(movieResponse.data.data)
+                setIsPending(false);
+            }
+        }
+        getData();
+    },[]);
     return ( 
-        <div>
-            Movie Componetn
-        </div>
+        <Scroll>
+            <Container className=" py-4 rounded">
+                {IsPending && <Loading />}
+                {Err && <ErrorComponent err={Err} />}
+                {
+                    movie &&
+                    <>
+                    <Row className="justify-content-center">
+                        <Col md={3} className="d-flex justify-content-center align-items-center border rounded border-2 mb-2 me-sm-2 border-light bg-light">
+                            <img src={`${baseImageURL}${movie.poster_path}`} alt={`Poster for ${movie.title}`} style={{maxWidth:"100%"}}/>
+                        </Col>
+                        <Col md={6} className="d-flex align-items-center p-4  border rounded border-2 mt-2 ms-sm-2 border-light bg-light">
+                            <div>                             
+                                <h2><b>{movie.title}</b>  <Badge bg="primary">{movie.vote_average}</Badge></h2>
+                                <p><i>{movie.tagline}</i></p>
+                                <p><b>Runtime : </b>{movie.runtime} mins</p>
+                                <p>
+                                    {movie.overview}
+                                </p>
+                                <p><b>Year</b> : {movie.release_date}  |  <b>Language</b> : <Badge bg="dark">{movie.original_language}</Badge></p>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12} className="px-4 border mt-4">
+                        <Stack direction="horizontal" gap={3}>
+                            <div className="display-6">Production Houses</div>
+                            <div className="vr" />
+                            <div className=" ms-auto">
+                                <Stack direction="horizontal" gap={3}>
+                                    {
+                                        movie &&
+                                        movie.production_companies?.map((company)=>(
+                                            <Card key={company.id} className="bg-light text-dark m-2" style={{maxWidth:"200px", maxHeight : "80px"}} >
+                                                <Card.Img src={`${baseImageURL}${company.logo_path}`} alt={`${company.name}'s Logo`} className="thumbnail" style={{maxWidth:"200px", maxHeight : "80px",objectFit:"scale-down"}} />
+                                                <Card.ImgOverlay className="center"> 
+                                                </Card.ImgOverlay>
+                                            </Card>
+                                        ))
+                                    }
+                                </Stack>
+                            </div>                        
+                        </Stack>               
+                                
+                        </Col>
+                    </Row>
+                    </>
+                }
+                    
+            </Container>
+        </Scroll>
      );
 }
  
